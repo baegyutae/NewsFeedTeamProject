f4dcea5532a4fdefccfc51b3657c78e170de8268
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-11-21 14:52:05 +0900

    init commit

---
+++ src/main/java/com/sparta/newsfeedteamproject/NewsfeedteamprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedteamprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedteamprojectApplication.class, args);
+    }
+
+}
